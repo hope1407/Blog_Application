f8490cc1264de57ac005bde089e7e996f2c12aa8
Hope <johnhope354@gmail.com>
2021-07-19 21:00:22 -0500

    added Comments class to model index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,23 @@
 const User = require('./User');
 const Blogs = require('./Blogs');
+const Comments = require('./Comments');
 
-User.hasMany(Blog, {
+User.hasMany(Blogs, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
   });
 
 Blogs.belongsTo(User, {
     foreignKey: 'user_id'
-})
+});
 
-module.exports = { User, Blogs };
+Blogs.hasMany(Comments, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+});
+
+Comments.belongsTo(Blogs, {
+  foreignKey: 'blog_id'
+});
+
+module.exports = { User, Blogs, Comments };
