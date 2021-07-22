dfd4d51877ad32510bf31c293e32f0b1062c3258
Hope <johnhope354@gmail.com>
2021-07-22 07:01:52 -0500

    moved sequelize variable from let to const

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+const sequelize;
 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(precess.env.JAWSDB_URL);
